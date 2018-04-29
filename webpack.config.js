6b0000ca9947de7f8d978c5adeac74add475f607
benjipelletier <pelletierbenji@gmail.com>
2018-04-29 13:28:19 -0400

    Added webpack/babel configuration

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+	entry: './src/app.jsx',
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	mode: 'development',
+	module: {
+		rules: [{
+			loader: 'babel-loader',
+			test: /\.jsx$/,
+			exclude: /node_modules/
+		}, {
+		test: /\.s?css$/,
+		use: [
+			'style-loader',
+			'css-loader',
+			'sass-loader'
+		]
+		}]
+	},
+	devtool: 'cheap-module-eval-source-map',
+	devServer: {
+		contentBase: path.join(__dirname, 'public')
+	}
+}
