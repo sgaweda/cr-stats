1ea7edaeb67f0881ed11f4ed3dcca642392b2a69
benjipelletier <pelletierbenji@gmail.com>
2018-05-03 23:29:07 -0400

    semantic/webpack updates

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# cr-stats
-clash royale stats site
+# Semantic UI Webpack Starter
+
+> Webpack-based Semantic UI seed project.
+
+## Notes
+
+Uses Webpack version 3.
+
+## Theme configuration and customization
+
+http://learnsemantic.com/developing/customizing.html
+
+Theme configuration and customization can be done inside the 'semantic-theme' directory.
+
+## Package management
+
+Both yarn and npm are supported.
+
+## Installation
+
+* Clone project
+* Install dependencies: `yarn install`
+
+## Scripts
+
+`yarn run dev`
+
+Start development server with hot reloading
+
+`yarn run test`
+
+Test code
+
+`yarn run build`
+
+Build code using Webpack
+
+`yarn run serve`
+
+Build code and serve it using http-server
+
+`yarn run clean`
+
+Clean dist folder
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,30 @@
-const path = require('path')
+const webpack = require('webpack');
+const path = require('path');
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const SRC = path.resolve(__dirname, './src');
+const DIST = path.resolve(__dirname, './public');
+
+const extractStyles = loaders => {
+  return process.env.NODE_ENV === 'production'
+    ? ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: loaders
+      })
+    : ['style-loader'].concat(loaders);
+};
 
 module.exports = {
-	entry: './src/app.jsx',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	mode: 'development',
-	module: {
-		rules: [{
+  entry: './src/app.jsx',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
 			loader: 'babel-loader',
 			test: /\.(js|jsx)$/,
 			exclude: /node_modules/
@@ -20,9 +36,30 @@ module.exports = {
 			'sass-loader'
 		]
 		}]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
 		contentBase: path.join(__dirname, 'public')
 	}
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    // Short-circuit all warning code.
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    // Minify with dead-code elimination.
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      },
+      parallel: true
+    }),
+    // Enable scope hoisting.
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ]);
 }
